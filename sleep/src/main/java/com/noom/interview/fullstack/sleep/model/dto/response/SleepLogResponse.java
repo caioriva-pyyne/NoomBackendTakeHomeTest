e8a1eb8bf9c455ee0397a6a22c721c72981dbb8e
Caio Riva <caio.riva@pyyne.com>
2024-09-23 21:12:03 -0300

    Add get average of last days sleep logs endpont

--- sleep/src/main/java/com/noom/interview/fullstack/sleep/model/dto/response/SleepLogResponse.java
+++ sleep/src/main/java/com/noom/interview/fullstack/sleep/model/dto/response/SleepLogResponse.java
@@ -1,6 +1,5 @@
 package com.noom.interview.fullstack.sleep.model.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.noom.interview.fullstack.sleep.model.entity.AfterSleepFeeling;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,18 +11,9 @@ import java.time.LocalTime;
 @Getter
 @AllArgsConstructor
 public class SleepLogResponse {
-    @JsonProperty
     private LocalDate sleepDate;
-
-    @JsonProperty
     private LocalTime timeInBedStart;
-
-    @JsonProperty
     private LocalTime timeInBedEnd;
-
-    @JsonProperty
     private Duration totalTimeInBed;
-
-    @JsonProperty
     private AfterSleepFeeling feeling;
 }
